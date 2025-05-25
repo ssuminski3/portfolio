d489c1e65353b72e3ca1535b7e0f91fa8204d3b8
ssuminski3 <ssuminski3@gmail.com>
2025-05-25 11:56:23 +0200

    Added database.

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,12 +1,22 @@
 // db.ts
 import { Pool } from 'pg';
 
-const pool = new Pool({
+//Local database
+
+/*const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+});*/
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 export default pool;
